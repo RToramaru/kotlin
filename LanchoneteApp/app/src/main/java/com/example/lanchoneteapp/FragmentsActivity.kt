670dbd7554fa070acc59c7e9821ea1c6170ac5ef
RToramaru <rafael.almeida.soares2012@gmail.com>
2021-05-23 17:27:17 -0300

    settings

--- LanchoneteApp/app/src/main/java/com/example/lanchoneteapp/FragmentsActivity.kt
+++ LanchoneteApp/app/src/main/java/com/example/lanchoneteapp/FragmentsActivity.kt
@@ -1,13 +1,27 @@
 package com.example.lanchoneteapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.View.inflate
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.res.ColorStateListInflaterCompat.inflate
+import androidx.core.graphics.drawable.DrawableCompat.inflate
 import androidx.fragment.app.Fragment
 import com.example.lanchoneteapp.fragments.CartFragment
 import com.example.lanchoneteapp.fragments.HistoryFragment
 import com.example.lanchoneteapp.fragments.HomeFragment
 import com.example.lanchoneteapp.fragments.SettingsFragment
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.activity_fragments.*
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 class FragmentsActivity : AppCompatActivity() {
 
@@ -15,6 +29,7 @@ class FragmentsActivity : AppCompatActivity() {
     private val cartFragment = CartFragment()
     private val historyFragment = HistoryFragment()
     private val settingsFragment = SettingsFragment()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,8 +44,8 @@ class FragmentsActivity : AppCompatActivity() {
             }
             true
         }
-    }
 
+    }
 
     private fun replaceFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
--- LanchoneteApp/app/src/main/java/com/example/lanchoneteapp/fragments/SettingsFragment.kt
+++ LanchoneteApp/app/src/main/java/com/example/lanchoneteapp/fragments/SettingsFragment.kt
@@ -1,21 +1,42 @@
 package com.example.lanchoneteapp.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.View.inflate
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import com.example.lanchoneteapp.R
+import com.example.lanchoneteapp.databinding.FragmentSettingsBinding
+import kotlinx.android.synthetic.main.fragment_settings.*
+
 
 
 class SettingsFragment : Fragment() {
 
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?
+    override fun onResume() {
+        super.onResume()
+        val languages = resources.getStringArray(R.array.languages)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, languages)
+        binding.autoCompleteTextView.setAdapter(arrayAdapter)
+    }
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_settings,container,false)
+        //Inflate the layout for this fragment
+        _binding = FragmentSettingsBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 
